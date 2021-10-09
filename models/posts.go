4139d3e0733a6f0b787918d8dbbe9fdf9a63bf9b
Jayant Verma <vjayant018@gmail.com>
2021-10-09 21:57:30 +0530

    Added all files

--- models/posts.go
+++ models/posts.go
@@ -2,7 +2,6 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-//Create Struct
 type Post struct {
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Caption   string             `json:"caption,omitempty" bson:"caption,omitempty"`
